c4ae8a620598569d0c24c16a16c590f40a7f9ea8
rodrigueszkkk <kaikyauthentic@gmail.com>
2026-09-06 16:39:48 -0300

    feat(ui): configure cupertino-dark slint style and smooth queue item hover transitions

--- build.rs
+++ build.rs
@@ -1,5 +1,7 @@
 fn main() {
-    slint_build::compile("ui/main.slint").unwrap();
+    let mut config = slint_build::CompilerConfiguration::new();
+    config = config.with_style("cupertino-dark".into());
+    slint_build::compile_with_config("ui/main.slint", config).unwrap();
     println!("cargo:rerun-if-changed=ui/main.slint");
     println!("cargo:rerun-if-changed=ui/sidebar.slint");
     println!("cargo:rerun-if-changed=ui/top_player.slint");
